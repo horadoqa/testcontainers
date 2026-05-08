ff720f42930113bbc44df8705a0cb6b99144bdb8
Ricardo Fahham <rfahham@hotmail.com>
2026-05-07 22:22:47 -0300

    Debugando biblioteca

--- tests/ci/libraries/postgres_container.py
+++ tests/ci/libraries/postgres_container.py
@@ -10,25 +10,33 @@ class PostgresContainerLibrary:
         self.conn = None
 
     def start_postgres_container(self):
-        self.postgres = PostgresContainer("postgres:16")
-        self.postgres.start()
+        print("STARTING POSTGRES CONTAINER")
 
-        self.conn = psycopg2.connect(
-            host=self.postgres.get_container_host_ip(),
-            port=self.postgres.get_exposed_port(5432),
-            database=self.postgres.dbname,
-            user=self.postgres.username,
-            password=self.postgres.password
-        )
+        try:
+            self.postgres = PostgresContainer("postgres:16")
+            self.postgres.start()
 
-        return {
-            "conn": self.conn,
-            "host": self.postgres.get_container_host_ip(),
-            "port": self.postgres.get_exposed_port(5432),
-            "dbname": self.postgres.dbname,
-            "user": self.postgres.username,
-            "password": self.postgres.password
-        }
+            self.conn = psycopg2.connect(
+                host=self.postgres.get_container_host_ip(),
+                port=self.postgres.get_exposed_port(5432),
+                database=self.postgres.dbname,
+                user=self.postgres.username,
+                password=self.postgres.password
+            )
+
+            print("CONTAINER STARTED SUCCESSFULLY")
+
+            return {
+                "conn": self.conn,
+                "host": self.postgres.get_container_host_ip(),
+                "port": self.postgres.get_exposed_port(5432),
+                "dbname": self.postgres.dbname,
+                "user": self.postgres.username,
+                "password": self.postgres.password
+            }
+        except Exception as e:
+            print(f"ERROR STARTING POSTGRES: {e}")
+            raise
 
     def insert_user(self, nome, data_nascimento, sexo, estado_civil, naturalidade, nacionalidade):
         with self.conn.cursor() as cur:
@@ -49,5 +57,3 @@ class PostgresContainerLibrary:
     def close_connection(self):
         if self.conn:
             self.conn.close()
-
-print("POSTGRES LIBRARY LOADED")
